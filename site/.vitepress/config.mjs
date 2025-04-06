85fd5ab36951d6bacd57e63390f3fe0064536bab
KurbiOPS <Pascal.steingruber@gmx.ch>
2025-04-06 19:56:31 +0200

    modified:   site/.vitepress/config.mjs

--- site/.vitepress/config.mjs
+++ site/.vitepress/config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Parabeln und Gerade', link: '/funktionen' }
+      { text: 'Parabeln und Gerade', link: '/Funktionen' }
     ],
 
     sidebar: [
